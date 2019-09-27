885ee6f7b5d0aa7c568f97b7f749ba0d6db58113
Caliduseb <46005624+Caliduseb@users.noreply.github.com>
2019-09-27 23:41:40 +0200

    added date return added sendPhoto void

--- telegram.py
+++ telegram.py
@@ -4,20 +4,25 @@ import json
 
 class Telegram:
 
-    def __init__(self, token):
+    def __init__(self, botID, token):
         self.token = token
+        self.botID = botID
 
     def sendMessage(self, chatID, botMessage):
-        send_text = 'https://api.telegram.org/bot' + self.token + '/sendMessage?chat_id=' + chatID + '&parse_mode=Markdown&text=' + botMessage
+        send_text = 'https://api.telegram.org/bot' + self.botID + ":" + self.token + '/sendMessage?chat_id=' + chatID + '&text=' + botMessage
         response = requests.get(send_text)
         return response.json()
 
-    def getLastMessage(self, botID):
-        checkMessage = "https://api.telegram.org/bot" + botID + ":" + self.token + "/getUpdates"
+    def sendPhoto(self, chatID, photo):
+        requests.post(str('https://api.telegram.org/bot' + self.botID + ":" + self.token) + '/sendPhoto', data={'chat_id': chatID}, files={'photo': photo})
+
+    def getLastMessage(self, date=False):
+        checkMessage = "https://api.telegram.org/bot" + self.botID + ":" + self.token + "/getUpdates"
         response = json.loads(requests.get(checkMessage).text)
         latest = response["result"][-1]
         text = latest["message"]["text"]
         sender = latest["message"]["from"]["id"]
-
+        if date:
+            return sender, text, str(latest["message"]["date"])
         return sender, text
 
