b325cdfefbdb02649f2198892aec82980fb69622
Caliduseb <46005624+Caliduseb@users.noreply.github.com>
2019-12-09 21:27:54 +0100

    Useabillity

--- GoogleCoinChallange.py
+++ GoogleCoinChallange.py
@@ -1,4 +1,4 @@
-Coins = {2.00: "2eur", 1: "1eur",0.50: "50ct", 0.20: "20ct", 0.10: "10ct", 0.05: "5ct", 0.02: "2ct", 0.01: "1ct"}
+Coins = {2.00: "2eur", 1: "1eur", 0.50: "50ct", 0.20: "20ct", 0.10: "10ct", 0.05: "5ct", 0.02: "2ct", 0.01: "1ct"}
 preis = float(input("rückgeld: "))
 imSinn = preis + 0.00001
 change = []
--- spammer.py
+++ spammer.py
@@ -8,24 +8,30 @@ time.sleep(1)
 minecraft = False
 f = open(input("path to dictionary > "), "r").readlines()
 
-while not keyboard.is_pressed("esc"):
+#while not keyboard.is_pressed("esc"):
 
-    if sys.argv[-1] != "r":
-        w = random.choice(f)
-        if minecraft:
-            keyboard.write("t")
-        time.sleep(0.3)
-        keyboard.write(w)
-        keyboard.press_and_release('enter')
-        time.sleep(0.3)
+while True:
+    input("> ")
 
-    else:
-        n = random.randint(1, 11)
-        f = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
+    for i in range(8):
+        #time.sleep(2)
 
-        if minecraft:
-            keyboard.press_and_release("t")
-        for i in range(f.__len__()):
-            t = f[i:i + 1]
-            keyboard.press_and_release(t)
-        keyboard.press_and_release("enter")
+        if sys.argv[-1] != "r":
+            w = random.choice(f)
+            if minecraft:
+                keyboard.write("t")
+            time.sleep(0.3)
+            keyboard.write(w)
+            keyboard.press_and_release('enter')
+            time.sleep(0.3)
+
+        else:
+            n = random.randint(1, 11)
+            f = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
+
+            if minecraft:
+                keyboard.press_and_release("t")
+            for i in range(f.__len__()):
+                t = f[i:i + 1]
+                keyboard.press_and_release(t)
+            keyboard.press_and_release("enter")
